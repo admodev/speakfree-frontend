c12ff99ede33e69883bde97da5c0a58742cfb0aa
admodev <admodevcodes@outlook.com>
2022-11-27 20:56:34 -0300

    add posts window to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,32 @@
-<script setup></script>
+<script setup>
+  import '98.css';
+</script>
 
 <template>
   <main>
-    <h1>Home</h1>
+    <div class="window mainWindow">
+      <div class="title-bar">
+        <div class="title-bar-text">Posts</div>
+        <div class="title-bar-controls">
+          <button aria-label="Minimize" />
+          <button aria-label="Maximize" />
+          <button aria-label="Close" />
+        </div>
+      </div>
+
+      <div class="window-body">
+        <h4>Username</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, aliquam repellat? Incidunt assumenda velit explicabo nam dolore unde eaque debitis. Consectetur sequi, accusantium necessitatibus sint minima cupiditate sed ab quidem.
+        </p>
+        <div class="field-row fieldRowDiv">
+          <input type="text" placeholder="Comment" />
+          <button class="loginButton">I like this</button>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+</style>
